7ee52c30fcf4ae9fc6f362a4551fbdc44006a60f
QWolf <peter.schroten@hotmail.com>
2013-11-20 12:34:37 +0100

    rec spul

--- Workspace/ss/src/ss/week2/Rectangle.java
+++ Workspace/ss/src/ss/week2/Rectangle.java
@@ -1,5 +1,30 @@
 package ss.week2;
 
 public class Rectangle {
+	
+	/*@ requires length > 0 && width > 0;
+	    ensures area > 0 && perimeter > 0;
+	*/
+	public Rectangle(int length, int width){
+		
+	}
+	
+	//@ invariant length() > 0;
+	public int length(){
+		
+	}
+	
+	//@ invariant width > 0;
+	public int width (){
+		
+	}
+	
+	public int area() {
+		
+	}
+	
+	public int perimeter(){
+		
+	}
 
 }
